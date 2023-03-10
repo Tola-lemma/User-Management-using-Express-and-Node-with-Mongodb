e96403182b720ae80d564947e6398cc2f5d15e02
Tola-lemma <tolalemma@gmail.com>
2023-03-10 14:43:00 +0300

    environment variables to access configuration information

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const dotenv = require('dotenv')
 const app = express()
 
+dotenv.config({path:'config.env'})
+const PORT = process.env.PORT || 8080
 
 app.get('/',(req,res) => {
    res.send('Welcome to crud Application ')
 });
-app.listen(3000,()=>console.log(`Server is running on http://localhost:${3000} ...`))
+app.listen(PORT,()=>console.log(`Server is running on http://localhost:${PORT} ...`))
