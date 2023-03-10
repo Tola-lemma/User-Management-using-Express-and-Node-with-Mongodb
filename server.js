ecaf72164098176156da4db48f1d37b98f28ba2e
Tola-lemma <tolalemma@gmail.com>
2023-03-10 14:57:33 +0300

    log request- morgan

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
+
 const app = express()
 
 dotenv.config({path:'config.env'})
 const PORT = process.env.PORT || 8080
-
+//log request
+app.use(morgan('tiny'))
 app.get('/',(req,res) => {
    res.send('Welcome to crud Application ')
 });
