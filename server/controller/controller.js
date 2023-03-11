131accab5e53c86e47b4be19c268a1d7ca5acd64
Tola-lemma <tolalemma@gmail.com>
2023-03-11 23:17:23 +0300

    new user created and saved to database

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,7 +1,26 @@
-var userdb = require('../model/model')
+var Userdb = require('../model/model')
 //create and save new user 
 exports.create = (req,res)=>{
-
+if(!req.body){
+    res.status(400).send({message:'Content cannot be empty!'})
+    return
+}
+//new user
+const user = new Userdb({
+    name: req.body.name,
+    email: req.body.email,
+    gender: req.body.gender,
+    status: req.body.status
+})
+//save user in database
+user
+  .save(user)
+  .then(data =>{
+    res.send(data);
+  })
+  .catch(err =>{
+    res.status(500).send({message:err.message || 'some error occurred while creating a create operation'})
+});
 }
 //retrieve and return all users / retrive and return single user
 exports.find= (req,res)=>{
@@ -13,5 +32,5 @@ exports.update = (req,res)=>{
 }
 //delete a user with specified user id
 exports.delete = (req,res)=>{
-    
+
 }
