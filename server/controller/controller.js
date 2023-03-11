6fd34b7a63bb2788b1bb2bfbe37e89161aaea574
Tola-lemma <tolalemma@gmail.com>
2023-03-11 23:49:00 +0300

    retrieving user information and updating user information is done

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -24,11 +24,33 @@ user
 }
 //retrieve and return all users / retrive and return single user
 exports.find= (req,res)=>{
-
+   Userdb.find()
+   .then(user =>{
+    res.send(user)
+   })
+   .catch(err =>{
+   res.status(500).send({message:err.message ||`Error while retrieving the user information`});
+   })
 }
 //update a new identified user by user id
 exports.update = (req,res)=>{
-
+  if(!req.body){
+     return res
+          .status(404)
+          .send({message:err.message ||`Error while updating the data cannot be empty`});
+  }
+  const id = req.params.id;
+  Userdb.findByIdAndUpdate (id,req.body,{useFindAndModify:false})
+       .then(data=>{
+        if(!data){
+            res.status(404).send({message:err.message ||`cannot update user with identified id ${id} or maybe user not found!`})
+        } else{
+            res.send(data);
+        }
+       })
+       .catch(err =>{
+    res.status(500).send({message: err.message || `error updating user information`});
+       })
 }
 //delete a user with specified user id
 exports.delete = (req,res)=>{
