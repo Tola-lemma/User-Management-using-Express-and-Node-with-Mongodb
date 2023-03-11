3849d0ea69d01e0a98f8ce429a0ded8f5498ff1c
Tola-lemma <tolalemma@gmail.com>
2023-03-12 01:08:40 +0300

    Api is passed in render function fetched by axios

--- server/services/render.js
+++ server/services/render.js
@@ -1,3 +1,12 @@
-exports.homeRoutes  =(req,res)=> res.render('index')
+const axios = require('axios');
+exports.homeRoutes  =(req,res)=> {
+    //make a get request to /api/users
+    axios.get('http://localhost:3000/api/users')
+           .then((res)=>{
+           res.render('index',{users:res.data})
+           })
+           .catch((err)=>res.send(err));
+    
+}
 exports.add_user  =(req,res)=> res.render('add_user')
 exports.update_user  =(req,res)=> res.render('update_user')
