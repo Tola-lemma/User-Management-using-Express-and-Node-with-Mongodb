b3a4e9a3a38cdfb31aaf1ed4f2a6006e61ee0bef
Tola-lemma <tolalemma@gmail.com>
2023-03-11 20:46:03 +0300

    deprecated options are removed from mongodb connection

--- server/database/connection.js
+++ server/database/connection.js
@@ -3,12 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async ()=>{
     try {
         //mongodb connection 
-        const conn  = await mongoose.connect(process.env.MONGO_URI,{
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false,
-            useCreateIndex:true
-        })
+        const conn  = await mongoose.connect(process.env.MONGO_URI)
         console.log(`MongoDB connected: ${conn.connection.host}`);
         
     } catch (err) {
