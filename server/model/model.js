a289aaeb16e5554545b9630cbe4367433dbe71f3
Tola-lemma <tolalemma@gmail.com>
2023-03-11 21:13:25 +0300

    mongodb schema is created (shape of document)

---
+++ server/model/model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+var schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  gender: String,
+  status: String,
+});
+ const userdb = mongoose.model('userdb',schema)
+ module.exports = userdb;
