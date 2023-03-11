6273f9b903f1afa8d24d6fc24c5f2f4d3023ff96
Tola-lemma <tolalemma@gmail.com>
2023-03-11 21:40:35 +0300

    the rest controller are added

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,5 +1,17 @@
 var userdb = require('../model/model')
 //create and save new user 
 exports.create = (req,res)=>{
+
+}
+//retrieve and return all users / retrive and return single user
+exports.find= (req,res)=>{
+
+}
+//update a new identified user by user id
+exports.update = (req,res)=>{
+
+}
+//delete a user with specified user id
+exports.delete = (req,res)=>{
     
 }
