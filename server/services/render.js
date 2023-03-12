c0a5392af30c00dc3a4ba59710224d1472869f16
Tola-lemma <tolalemma@gmail.com>
2023-03-12 11:27:49 +0300

    update user url is fetched using axios and id is passes in the router

--- server/services/render.js
+++ server/services/render.js
@@ -9,4 +9,10 @@ exports.homeRoutes  =(req,res)=> {
     
 }
 exports.add_user  =(req,res)=> res.render('add_user')
-exports.update_user  =(req,res)=> res.render('update_user')
+exports.update_user  =(req,res)=> {
+    axios.get('http://localhost:3000/api/users',{params:{id:req.query.id}})
+      .then((userdata)=>{
+        res.render('update_user',{user:userdata.data})
+      })
+      .catch((err)=>{res.send(err)});
+}
